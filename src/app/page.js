e1a19a66cdc64ef996949ff1ea474668a08beebd
dixit.soni <dixit.soni@artoon.in>
2023-09-16 16:39:14 +0530

    UPdate page.js

--- src/app/page.js
+++ src/app/page.js
@@ -4,13 +4,13 @@ import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
+      {/* <div className={styles.description}> */}
+        {/* <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
+        </p> */}
+        {/* <div> */}
+          {/* <a
             href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -89,7 +89,7 @@ export default function Home() {
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
-      </div>
+      </div> */}
     </main>
   )
 }
